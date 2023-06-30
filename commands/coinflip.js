93c4eecdfc63a499768873b6c3d2967de3f3cfd1
BrandgrandRealMe <47084010+BrandgrandRealMe@users.noreply.github.com>
2023-06-30 20:25:31 +0000

    Added Coinflip

---
+++ commands/coinflip.js
@@ -0,0 +1,13 @@
+const { CommandBuilder } = require("../Commands.js");
+const Embed = require("../util/embed");
+
+module.exports = {
+  command: new CommandBuilder()
+    .setName("coinflip")
+    .setDescription("Flip a coin."),
+  run: async function(msg, data) {
+  let items = ['Heads', 'Tails'];
+  let random = items[Math.floor(Math.random() * items.length)];
+  msg.reply(random)
+  }
+}
